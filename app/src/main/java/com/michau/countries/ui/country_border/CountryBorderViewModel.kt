cba4a614833901c5fc26bcb0c7543455e4f7ccbf
Michauuek <michal.rzemieniuk@gmail.com>
2023-02-20 11:52:06 +0100

    Add calculate direction based on angle

--- app/src/main/java/com/michau/countries/ui/country_border/CountryBorderViewModel.kt
+++ app/src/main/java/com/michau/countries/ui/country_border/CountryBorderViewModel.kt
@@ -52,21 +52,34 @@ class CountryBorderViewModel @Inject constructor(
                         .getDetailsByCountryName(event.country)
                         .data?.first()?.toCountryModel()
 
-                    val distance = try {
-                        calculateDistanceInKilometer(
+                    var distance: Int? = null
+                    var angle: Double? = null
+
+                    try {
+                        distance = calculateDistanceInKilometer(
                             currentCountry?.latlng?.component1()!!,
                             currentCountry?.latlng?.component2()!!,
                             guessedCountry?.latlng?.component1()!!,
                             guessedCountry.latlng.component2()
                         )
+                        angle = angleFromCoordinate(
+                            currentCountry?.latlng?.component1()!!,
+                            currentCountry?.latlng?.component2()!!,
+                            guessedCountry.latlng.component1(),
+                            guessedCountry.latlng.component2()
+                        )
                     } catch (e: IndexOutOfBoundsException) {
-                        null
+                        e.printStackTrace()
                     }
 
                     if (distance == 0) {
                         sendUiEvent(UiEvent.ShowToast("You've guessed correctly"))
                     } else {
-                        sendUiEvent(UiEvent.ShowToast("Wrong answer - $distance"))
+                        sendUiEvent(
+                            UiEvent.ShowToast("Wrong answer distance: " +
+                                    "$distance angle: ${calculateDirection(angle ?: -1.0)}"
+                            )
+                        )
                     }
                 }
             }
@@ -96,6 +109,57 @@ class CountryBorderViewModel @Inject constructor(
         val c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
         return (AVERAGE_RADIUS_OF_EARTH_KM * c).roundToInt()
+    }
+
+    /**
+     *  Function to calculate angle between 2 locations
+     *  using latitude and longitude
+     */
+    private fun angleFromCoordinate(
+        userLat: Double, userLng: Double,
+        venueLat: Double, venueLng: Double
+    ): Double {
+
+        val lat1 = Math.toRadians(userLat)
+        val lat2 = Math.toRadians(venueLat)
+        val long1 = Math.toRadians(userLng)
+        val long2 = Math.toRadians(venueLng)
+
+        val dLon = long1 - long2
+
+        val y = sin(dLon) * cos(lat2)
+        val x = cos(lat1) * sin(lat2) - (sin(lat1) * cos(lat2) * cos(dLon))
+
+        var brng = atan2(y, x)
+
+        brng = Math.toDegrees(brng)
+        brng = (brng + 360) % 360
+        brng = 360 - brng // count degrees counter-clockwise - remove to make clockwise
+
+        return brng
+    }
+
+    private fun calculateDirection(angle: Double): String {
+        return when(angle) {
+            in 0.0..11.25 -> "north"
+            in 11.26..33.75 -> "north of north-east"
+            in 33.76..56.25 -> "north-east"
+            in 56.26..78.75 -> "east-northeast"
+            in 78.76..101.25 -> "east"
+            in 101.26..123.75 -> "east-southeast"
+            in 123.76..146.25 -> "south-east"
+            in 146.26..168.75 -> "south-southeast"
+            in 168.76..191.25 -> "south"
+            in 191.26..213.75 -> "south-southwest"
+            in 213.76..236.25 -> "south-west"
+            in 236.26..258.75 -> "west-south-west"
+            in 258.76..281.25 -> "west"
+            in 281.26..303.75 -> "west-northwest"
+            in 303.76..326.25 -> "north-west"
+            in 326.26..348.67 -> "north-northwest"
+            in 348.75..360.0 -> "north"
+            else -> "unknown"
+        }
     }
 
     private fun sendUiEvent(event: UiEvent) {
